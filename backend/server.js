8fcd6bcb75301c8b918bea1843b0ca5e3dedfc99
Akanksha7890 <akankshaupadhyay136@gamil.com>
2026-02-05 10:01:03 +0530

    fix cors for local and render frontend

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,21 @@ import imageRouter from './routes/imageRoutes.js' // ✅ EXACT FILE NAME
 const PORT = process.env.PORT || 4000
 const app = express()
 
-app.use(express.json())
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.FRONTEND_URL
+]
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true)
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
   credentials: true
 }))
 
